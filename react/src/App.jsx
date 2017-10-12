e926e9475fc2d096225ff06413cb83c735760f22
mssen <missy.danielsen@gmail.com>
2017-10-12 15:53:09 -0500

    Clear and subimt work.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -13,6 +13,8 @@ export default class App extends React.Component {
     this.nextId = 1;
 
     this.addVictim = this.addVictim.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   addVictim(newVictimName) {
@@ -24,6 +26,20 @@ export default class App extends React.Component {
     }));
   }
 
+  handleClear() {
+    this.setState({
+      victims: []
+    });
+  }
+
+  handleSubmit(event) {
+    const ids = Object.keys(this.state.victims);
+    const randomId = ids[Math.floor(Math.random() * ids.length)];
+    const theChosen = this.state.victims[randomId];
+    alert(`It's ${theChosen.name}'s turn!`);
+    this.handleClear();
+  }
+
   render() {
     return (
       <div>
@@ -32,8 +48,15 @@ export default class App extends React.Component {
         </h1>
 
         <AddVictim addVictim={ this.addVictim } />
-        
         <VictimList victims={ this.state.victims } />
+
+        {
+          this.state.victims.length > 0
+            && <div>
+                  <button onClick={ this.handleClear }>Clear it!</button>
+                  <button onClick={ this.handleSubmit }>Churn it!</button>
+               </div>
+        }
       </div>
     )
   }
