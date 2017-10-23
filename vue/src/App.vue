40adcbd707a0d810bc5946483989233a3ae4efc2
mssen <missy.danielsen@gmail.com>
2017-10-23 15:47:12 -0500

    Finished implementation with add victim.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,30 +1,103 @@
 <template>
-  <div>
-    <h1>Greetings!</h1>
-    <victim-list :victims="victims"></victim-list>
+  <div class="main-container">
+    <h1 class="title">
+      Turn<em class="fancy">Churn</em>
+    </h1>
+
+    <add-victim v-on:add-victim="addVictim"></add-victim>
+    <victim-list :victims="victims" v-on:delete-victim="deleteVictim"></victim-list>
+
+    <div v-show="victims.length > 0" class="submit-container">
+      <button class="clear-button" v-on:click="clear">Clear it!</button>
+      <button class="submit-button" v-on:click="submit">Churn it!</button>
+    </div>
   </div>  
 </template>
 
 <script charset="utf-8">
+import AddVictim from './components/AddVictim';
 import VictimList from './components/VictimList';
 
 export default {
   components: {
+    AddVictim,
     VictimList
   },
   data() {
     return {
-      victims: [
-        { id: 1, name: 'One'},
-        { id: 2, name: 'Two'}
-      ]
+      victims: [],
+      nextId: 1
+    }
+  },
+  methods: {
+    addVictim(victimName) {
+      this.victims.push({ id: this.nextId++, name: victimName });
+    },
+    deleteVictim(victim) {
+      const victimIndex = this.victims.indexOf(victim);
+      this.victims.splice(victimIndex, 1);
+    },
+    clear() {
+      this.victims = [];
+    },
+    submit() {
+      const ids = Object.keys(this.victims);
+      const randomId = ids[Math.floor(Math.random() * ids.length)];
+      const theChosen = this.victims[randomId];
+      alert(`It's ${theChosen.name}'s turn!`);
+      this.clear();
     }
   }
 }
 </script>
 
 <style>
-h1 {
-  color: red;
+/* Colors: https://coolors.co/2d3142-4f5d75-bfc0c0-ffffff-ef8354 */
+
+body {
+  margin: 0;
+  padding: 0;
+  color: #2D3142;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.title {
+  font-size: 50px;
+}
+
+.fancy {
+  color: #EF8354;
+}
+
+.main-container {
+  display: flex;
+  align-items: center;
+  flex-flow: column;
+  height: 100vh;
+  background: white;
+}
+
+.submit-container {
+  display: flex;
+  width: 250px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.clear-button, .submit-button {
+  cursor: pointer;
+  border: 2px solid #EF8354;
+  border-radius: 2px;
+  padding: 10px 15px;
+}
+
+.clear-button {
+  color: #EF8354;
+  background: white;
+}
+
+.submit-button {
+  color: white;
+  background: #EF8354;
 }
 </style>
---
+++ vue/src/components/AddVictim.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="add-container">
+    <input
+      v-model="victim"
+      @keyup.enter="addVictim"
+      class="victim-input"
+      type="text"
+      placeholder="The next victim..."
+      autoFocus />
+    <button class="add-button" @click="addVictim">+ Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      victim: ''
+    }
+  },
+  methods: {
+    addVictim() {
+      this.$emit('add-victim', this.victim);
+      this.victim = '';
+    }
+  }
+}
+</script>
+
+<style>
+.add-container {
+  display: flex;
+}
+
+.add-button, .victim-input {
+  border: 2px solid #4F5D75;
+}
+
+.victim-input {
+  flex: 1;
+  border-right: 0;
+  border-radius: 2px 0 0 2px;
+  padding: 10px 15px;
+}
+
+.add-button {
+  cursor: pointer;
+  border-radius: 0 2px 2px 0;
+  color: white;
+  background: #4F5D75;
+}
+</style>
