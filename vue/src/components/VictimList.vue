09d85d7103cd4dec44aa9fc3b02ae07ec3a0c27a
mssen <missy.danielsen@gmail.com>
2017-10-23 15:45:16 -0500

    Shorthand for v-on.

--- vue/src/components/VictimList.vue
+++ vue/src/components/VictimList.vue
@@ -1,15 +1,47 @@
 <template>
-  <ul>
-    <li v-for="victim in victims" :key="victim.id">{{ victim.name }}</li>
-  </ul>  
+  <ul class="victim-list">
+    <li class="victim" v-for="victim in victims" :key="victim.id">
+      {{ victim.name }}
+      <button class="victim-delete" @click="deleteVictim(victim)">✕</button>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-  props: ['victims']
+  props: ['victims'],
+  methods: {
+    deleteVictim(victim) {
+      this.$emit('delete-victim', victim);
+    }
+  }
 }
 </script>
 
 <style>
+.victim-list {
+  margin: 30px 0;
+  padding: 0;
+  list-style: none;
+}
 
+.victim {
+  display: flex;
+  width: 250px;
+  margin: 10px 0;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.victim-delete {
+  background: transparent;
+  cursor: pointer;
+  border: 0;
+}
+
+.victim-delete:hover {
+  border-radius: 2px;
+  color: white;
+  background: #A51616;
+}
 </style>
